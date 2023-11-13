d052504f1c2aa1c4049239e00c800de367cbef33
Andre Ranussi <ranussi.a@gmail.com>
2023-11-13 13:25:11 +0000

    implemented the BMI Calcualtor Functonality

--- app/src/main/java/com/course/stretchesapp/BmiCalculatorActivity.kt
+++ app/src/main/java/com/course/stretchesapp/BmiCalculatorActivity.kt
@@ -21,9 +21,10 @@ class BmiCalculatorActivity : AppCompatActivity() {
     private var bmiResult: Double? = null
     private var result: String? = null
     private var resultMessage: String? = null
+    private var fieldCheckerFlag = false
 
-    private var etHeight: Editable? = null
-    private var etWeight: Editable? = null
+    private var etHeight = binding?.etHeight?.text.toString()
+    private var etWeight = binding?.etWeight?.text.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +41,8 @@ class BmiCalculatorActivity : AppCompatActivity() {
             finish()
         }
 
-        binding?.btnBmi?.setOnClickListener() {
+        binding?.btnBmi?.setOnClickListener {
+            fieldCheckerFlag = checkFields()
             calculateBmi()
 
         }
@@ -48,13 +50,43 @@ class BmiCalculatorActivity : AppCompatActivity() {
     }
 
 
+
     private fun calculateBmi() {
-           if (etHeight!!.isNotEmpty() || etWeight!!.isNotEmpty()) {
-            height = etHeight.toString().toDouble()
-            weight = etWeight.toString().toDouble()
+
+        if (fieldCheckerFlag) {
+
+            // 1.Implement BMI Calculation and display result
+            height = etHeight.toDouble()
+            weight = etWeight.toDouble()
             bmiResult =
                 (weight!! / (((height!! * height!!) * 100).roundToInt() / 100.0) * 100).roundToInt() / 100.0
             binding?.tvBmi?.text = bmiResult!!.toString()
+
+
+            // 2. Conditional for the BMI category and display it
+
+           result = when {
+                bmiResult!! < 18.5 -> "Category: Underweight"
+                bmiResult!! in 18.5..24.9 -> "Category: Healthy"
+                bmiResult!! in 25.0..29.9 -> "Category: Overweight"
+                bmiResult!! in 30.0..39.9 -> "Category: Obese"
+                bmiResult!! <= 40.0 -> "Category: Severely Obese"
+               else -> "please try again!"
+           }
+            binding?.tvBmiResult?.text = result
+
+            // 3. Conditional for BMI message and display it
+
+            binding?.tvBmiResultMessage?.text =
+                when (result) {
+                    "Category: Underweight" -> "Oops! You really need to take better care of yourself! Eat more!"
+                    "Category: Healthy" -> "Congratulations! You are in a good shape!"
+                    "Category: Overweight" -> "Oops! You need to take care of your yourself! Workout maybe!"
+                    "Category: Obese" -> "Oops! You really need to take care of your yourself! Workout maybe!"
+                    "Category: Severely Obese" ->  "OMG! You are in a very dangerous condition! Act now!"
+                    else -> " "
+
+                }
 
             binding?.tvYourBmi?.visibility = View.VISIBLE
             binding?.tvBmi?.visibility = View.VISIBLE
@@ -63,15 +95,27 @@ class BmiCalculatorActivity : AppCompatActivity() {
         } else {
             binding?.tvBmiResultMessage?.text = "Please enter a valid value for Weight and Height"
             binding?.tvBmiResultMessage?.visibility = View.VISIBLE
-        }
 
+        }
     }
+
+
+    private fun checkFields(): Boolean {
+        etHeight = binding?.etHeight?.text.toString()
+        etWeight = binding?.etWeight?.text.toString()
+
+        if(etWeight.isEmpty()) return false
+        if(etHeight.isEmpty()) return false
+    return true
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
 
         binding = null
     }
+
 }
 
 
